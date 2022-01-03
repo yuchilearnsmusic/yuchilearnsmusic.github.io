6486f3aad6309aaf0985fffd9bf7e56750d11d5d
Felicity Tian <you@example.com>
2022-01-03 15:59:12 +1100

    responsive blue card

--- script.js
+++ script.js
@@ -9,6 +9,8 @@ let card = document.querySelectorAll(".card");
 let imgIcon = document.querySelectorAll(".img-icon");
 let imgPicture = document.querySelectorAll(".img-picture");
 let titleTop = document.querySelectorAll(".title-top");
+let imageFullCircle = document.querySelectorAll(".img-full-circle");
+let description = document.querySelectorAll(".description-text");
 
 let titleOfCard = document.querySelectorAll(".title-of-card");
 
@@ -97,10 +99,11 @@ function removeActive() {
 
 /* ----------------------- RESPONSIVE CARDS BEGINS --------------------------*/
 
-
-
+console.log(width / height);
 
 if (width / height < 0.61) {
+
+    /* Big Card Begins */
 
     imgPicture.forEach((item) => {
         item.style.width = "65vw";
@@ -110,8 +113,21 @@ if (width / height < 0.61) {
         item.style.height = "65vh";
     })
 
+    imageFullCircle.forEach((item) => {
+        item.style.width = "59vw";
+    })
+
+    description.forEach((item) => {
+        item.style.fontSize = "3vw";
+    })
+
     phone();
+
+    /* Big Card Ends */
+
 } 
+
+// rest are new imageFullCircle 
 
 //tablet width
 
@@ -119,6 +135,10 @@ else if (width / height < 0.7) {
 
     imgPicture.forEach((item) => {
         item.style.width = "63vw";
+    })
+
+    description.forEach((item) => {
+        item.style.fontSize = "3vw";
     })
 
     phone();
@@ -132,12 +152,43 @@ else if (width / height < 0.8){
         item.style.width = "60vw";
     })
 
+    imageFullCircle.forEach((item) => {
+        item.style.width = "50vw";
+    })
+
+    description.forEach((item) => {
+        item.style.fontSize = "3vw";
+    })
+
     phone();
 } 
 
 /* computer dimensions */
 
-else if (width / height < 1.4) {
+else if (width / height < 1) {
+
+    imgPicture.forEach((item) => {
+        item.style.width = "34vw";
+    })
+
+    phone();
+
+    imgIcon.forEach((item) => {
+        item.style.width = "18vw";
+    })
+    titleOfCard.forEach((title) => {
+        title.style.fontSize = "5.5vw";
+    })
+
+    description.forEach((item) => {
+        item.style.fontSize = "3vw";
+    })
+
+
+} 
+
+else if (width / height < 1.3) {
+    phone();
     titleOfCard.forEach((title) => {
         title.style.fontSize = "5.5vw";
     })
@@ -149,9 +200,37 @@ else if (width / height < 1.4) {
     imgIcon.forEach((item) => {
         item.style.width = "10vw";
     })
+
+    description.forEach((item) => {
+        item.style.fontSize = "3vw";
+    })
+} 
+
+else if (width / height < 1.4) {
+    phone();
+    titleOfCard.forEach((title) => {
+        title.style.fontSize = "5vw";
+    })
+
+    imgPicture.forEach((item) => {
+        item.style.width = "30vw";
+    })
+
+    imageFullCircle.forEach((item) => {
+        item.style.width = "29vw";
+    })
+
+    imgIcon.forEach((item) => {
+        item.style.width = "10vw";
+    })
+
+    description.forEach((item) => {
+        item.style.fontSize = "3vw";
+    })
 } 
 
 else if (width / height < 1.71) {
+    phone();
     titleOfCard.forEach((title) => {
         title.style.fontSize = "3.75vw";
     })
@@ -160,8 +239,22 @@ else if (width / height < 1.71) {
         item.style.width = "28vw";
     })
 
+    imageFullCircle.forEach((item) => {
+        item.style.width = "23vw";
+    })
+
+    description.forEach((item) => {
+        item.style.fontSize = "2.5vw";
+    })
+
     imgIcon.forEach((item) => {
         item.style.width = "8.5vw";
+    })
+
+    card.forEach((item) => {
+        item.style.padding = "4vw";
+        item.style.borderRadius = "8vw";
+
     })
 }
 
@@ -176,6 +269,14 @@ else if (width / height < 1.81) {
 
     imgIcon.forEach((item) => {
         item.style.width = "8.5vw";
+    })
+
+    imageFullCircle.forEach((item) => {
+        item.style.width = "22vw";
+    })
+
+    description.forEach((item) => {
+        item.style.fontSize = "3vw";
     })
 }
 
